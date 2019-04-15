0f02687f40534b884f3676a3fc00b6acddcd9eab
gravic <grant@gravicdesign.com>
2019-04-15 10:34:06 -0700

    added alerts and messaging functionality

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,11 @@
 /*jshint esversion: 6 */
 import { combineReducers } from "redux";
 import leads from "./leads";
+import errors from "./errors";
+import messages from "./messages";
 
 export default combineReducers({
-  leads: leads
+  leads: leads,
+  errors: errors,
+  messages: messages
 });
